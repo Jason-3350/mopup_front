9bef927fa9340900218914ff5983bbb5aeb9ed92
jason3350 <liangying3350@gmail.com>
2022-04-02 21:18:28 +0100

    add token store info

--- src/store/index.js
+++ src/store/index.js
@@ -1,52 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import instance from "../utils/request";
 //1.安装vuex
 Vue.use(Vuex);
 //2.创建store对象
 const store = new Vuex.Store({
   state: {
-    // 所有待办目标
-    todos: [
-      {id: "001", event: "Running", location: "Running Building", start: "17:00", end: "18:00", date: "2022-01-01"},
-    ],
-    // 所有普通奖励
-    allRewards: [
-      {id: 1, coin: 5, name: "Aberdeen Books"},
-      {id: 2, coin: 10, name: "Supermarket"},
-      {id: 3, coin: 15, name: "Starbucks"},
-    ],
-    // 推荐奖励
-    // recoms: [
-    //   {id: 1, coin: 20, name: "Collaboration with business"},
-    //   {id: 2, coin: 10, name: "Sport Village"},
-    // ],
-    // 保存用户名
-    // user: {username: 'Dianna J. Thompson'},
-    user: {},
+    token: localStorage.getItem('token'),
+    refreshToken: localStorage.getItem('refresh'),
+    userID: "", //Login.vue
+    user: {id: "", username: "", email: ""}, //Menus.vue
+    coins: {id: "", coin: "", user_id: ""}, // searchHeader.vue
+    today: "",
+    order: {id: "", coin: "", rewards: "", qr: "", user: ""}, //order.vue
   },
   mutations: {
-    // 定义方法添加新的todo
-    addTodo(state, obj) {
-      state.todos.push(obj);
+    //将token保存到localStorage里，token表示登陆状态
+    setToken(state, token) {
+      state.token = token;
+      localStorage.setItem('token', token);
+      // console.log('store token: ' + token);
     },
-    // 删除现有的事件
-    removeTodo(state) {
-      // console.log(state.todos[id])
-      state.todos.pop();
+    refreshToken(state, refresh) {
+      state.refreshToken = refresh;
+      localStorage.setItem('refresh', refresh);
+      // console.log('refresh: ' + refresh);
     },
-    // 添加普通奖励
-    addReward(state, obj) {
-      state.allRewards.push(obj);
+    logout(state) {
+      state.token = null;
+      state.refreshToken = null;
+      localStorage.removeItem('token');
+      localStorage.removeItem('refresh');
+      sessionStorage.removeItem('store');
     },
-    // 添加推荐奖励
-    addRecom(state, obj) {
-      state.recoms.push(obj);
-    },
-    // 修改用户名
-    updateUser(state, userObj) {
-      state.user = userObj
-    }
-  }
+  },
 });
 //3.暴露store对象
 export default store;
