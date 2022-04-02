3511e7611ef64e70561067156542fd7dfaeee26c
jason3350 <liangying3350@gmail.com>
2022-04-02 21:06:39 +0100

    update notice.vue

--- src/views/Notice/Notice.vue
+++ src/views/Notice/Notice.vue
@@ -1,23 +1,35 @@
 <template>
   <div class="row no-gutters">
-    <ul class="list-group col-12">
-      <li class="list-group-item">An item</li>
-      <li class="list-group-item">A second item</li>
-      <li class="list-group-item">A third item</li>
-      <li class="list-group-item">A fourth item</li>
-      <li class="list-group-item">And a fifth one</li>
+    <ul class="list-group col-12" v-for="(notice,index) in notices" :key="notice.id">
+      <li class="list-group-item">{{ notice.notice }}</li>
     </ul>
   </div>
 </template>
 
 <script>
+import instance from "../../utils/request";
+
 export default {
-  name: "Notice"
+  name: "Notice",
+  data() {
+    return {
+      notices: {id: "", notice: ""},
+    }
+  },
+  created() {
+    // const userID = JSON.parse(localStorage.getItem('user')).id
+    const userID = this.$store.state.userID
+    instance.get('/notice/'+userID).then(res => {
+      this.notices = res.data;
+    }).catch(err => {
+      console.log(err);
+    })
+  }
 }
 </script>
 
 <style scoped>
-*{
+* {
   padding: 1rem 0;
   font-size: 2rem;
 }
